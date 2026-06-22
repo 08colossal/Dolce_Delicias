2ce914ad03e603a687b82bb72592e1603ae66fd7
henrique4575 <henriquesilvabrandao@gmail.com>
2026-06-22 13:29:20 -0300

    atualização na tela home e sobre
    atualização na tela home e sobre

--- Sobre/script.js
+++ Sobre/script.js
@@ -77,3 +77,24 @@ carrossel.addEventListener('mousemove', (e) => {
   const andar = (x - localInicioX) * 2; // Multiplica por 2 para velocidade do arraste
   carrossel.scrollLeft = rolagemEsquerda - andar;
 });
+
+/* ==========================================================================
+   SEÇÃO: CONFIGURAÇÃO DO DARK MODE (SCRIPT)
+========================================================================== */
+const botao = document.getElementById("theme-toggle");
+const icon = document.querySelector(".icon");
+
+botao.addEventListener("click", () => {
+    document.body.classList.toggle("light-theme");
+    icon.classList.add("animate");
+
+    if(document.body.classList.contains("light-theme")){
+        icon.textContent = "☀️";
+    }
+    else{
+        icon.textContent = "🌙";
+    }
+    setTimeout(() => {
+        icon.classList.remove("animate");
+    },700);
+});
