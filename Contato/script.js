d019122b46ad409e11804d8af5029dfd1023e5c7
Giovana Pauvels <pauvelsegiovana@gmail.com>
2026-06-22 03:14:34 -0300

    contato-darkmode

---
+++ Contato/script.js
@@ -0,0 +1,21 @@
+﻿
+/* ==========================================================================
+   SEÇÃO: CONFIGURAÇÃO DO DARK MODE (SCRIPT)
+========================================================================== */
+const botao = document.getElementById("theme-toggle");
+const icon = document.querySelector(".icon");
+
+botao.addEventListener("click", () => {
+    document.body.classList.toggle("light-theme");
+    icon.classList.add("animate");
+
+    if(document.body.classList.contains("light-theme")){
+        icon.textContent = "☀️";
+    }
+    else{
+        icon.textContent = "🌙";
+    }
+    setTimeout(() => {
+        icon.classList.remove("animate");
+    },700);
+});
